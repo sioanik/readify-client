fc8397030078db83410ed5f61e74eee06cef6b97
sioanik <sio.anik.007@gmail.com>
2024-05-10 18:36:50 +0600

    updated login page img

--- src/pages/user/Login.jsx
+++ src/pages/user/Login.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { AuthContext } from "./AuthProvider";
 import { toast } from "react-toastify";
 import { useLocation, useNavigate } from "react-router-dom";
+import loginimg from '../../assets/book-login-img.jpg'
 
 
 const Login = () => {
@@ -48,8 +49,8 @@ const Login = () => {
 
     return (
         <div>
-            <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
-                <div className="hidden bg-cover lg:block lg:w-1/2" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')" }}></div>
+            <div className="flex w-full max-w-sm mx-auto overflow-hidden rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
+                <div className="hidden bg-cover lg:block lg:w-1/2" style={{ backgroundImage: `url(${loginimg})` }}></div>
 
                 <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
                     <div className="flex justify-center mx-auto">
