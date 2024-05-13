590ea11f5dd32892becfcc4aa1d41df95f04924b
sioanik <sio.anik.007@gmail.com>
2024-05-13 11:21:57 +0600

    added list/grid view for all books sec

--- src/pages/allBooks/AllBooks.jsx
+++ src/pages/allBooks/AllBooks.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import Rating from "react-rating";
 import { Link, useLoaderData } from "react-router-dom";
+import { FaListAlt } from "react-icons/fa";
+import { BsFillGridFill } from "react-icons/bs";
+
+
 
 
 const AllBooks = () => {
@@ -22,43 +26,158 @@ const AllBooks = () => {
         setBooks(availableBooks)
     }
 
+
+    // grid/list view 
+
+    const [view, setView] = useState('grid')
+
+    const handleToggleView = (e) => {
+        if (e.target.checked) {
+            setView('list')
+        }
+        else {
+            setView('grid')
+        }
+    }
+    console.log(view);
+
+
+
     return (
         <div>
-            <div className="flex justify-center mb-20">
-                <button onClick={handleAvailable} className="btn btn-neutral">Show Only Available</button>
+            <div className="">
+
+                <div className="flex justify-end mr-20 ">
+                    <label className="flex cursor-pointer gap-2">
+                        <div className="flex items-center">
+                            <BsFillGridFill />
+                        </div>
+                        <input onChange={handleToggleView} type="checkbox" value="view" className="toggle theme-controller" />
+                        <div className="flex items-center">
+                            <FaListAlt />
+                        </div>
+                    </label>
+                </div>
+
+                <div className="flex justify-center mb-20">
+                    <button onClick={handleAvailable} className="btn btn-neutral">Show Only Available</button>
+                </div>
             </div>
-            <div className="grid grid-cols-2 gap-20 mx-20">
-                {
-                    books.map((item, idx) =>
-                        <div className="" key={idx}>
-                            <div className="border-4 overflow-hidden  rounded-lg shadow-lg">
-                                <div className="px-4 py-2">
-                                    <h1 className="text-xl font-bold text-gray-800 uppercase dark:text-white">{item.name}</h1>
-                                    <p className="mt-1 text-base text-gray-600 dark:text-gray-400">{item.category}</p>
-                                    <p className="mt-1 text-sm text-gray-600 dark:text-gray-400"><span>Author- </span>{item.author}</p>
-                                </div>
+            <div>
+                {view == 'grid' ? (
+                    <div>
 
-                                <img className="object-contain w-full h-48 mt-2" src={item.image} alt="" />
 
-                                <div className="flex items-center justify-between px-4 py-2 bg-slate-300">
-                                    {/* <h1 className="text-lg font-bold text-white"></h1> */}
-                                    <div className="">
-                                        <Rating
-                                            initialRating={item.rating}></Rating>
+                        <div className="grid grid-cols-2 gap-20 mx-20">
+                            {
+                                books.map((item, idx) =>
+                                    <div className="" key={idx}>
+                                        <div className="border-4 overflow-hidden  rounded-lg shadow-lg">
+                                            <div className="px-4 py-2">
+                                                <h1 className="text-xl font-bold text-gray-800 uppercase dark:text-white">{item.name}</h1>
+                                                <p className="mt-1 text-base text-gray-600 dark:text-gray-400">{item.category}</p>
+                                                <p className="mt-1 text-sm text-gray-600 dark:text-gray-400"><span>Author- </span>{item.author}</p>
+                                            </div>
+
+                                            <img className="object-contain w-full h-48 mt-2" src={item.image} alt="" />
+
+                                            <div className="flex items-center justify-between px-4 py-2 bg-slate-300">
+                                                {/* <h1 className="text-lg font-bold text-white"></h1> */}
+                                                <div className="">
+                                                    <Rating
+                                                        initialRating={item.rating}></Rating>
+                                                </div>
+                                                <Link to={`/update-book/${item._id}`}>
+                                                    <button className="px-2 py-1 text-lg font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-accent rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">Update</button>
+                                                </Link>
+                                            </div>
+                                        </div>
+
                                     </div>
-                                    <Link to={`/update-book/${item._id}`}>
-                                        <button className="px-2 py-1 text-lg font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-accent rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">Update</button>
-                                    </Link>
-                                </div>
+                                )
+                            }
+                        </div>
+
+                    </div>)
+                    :
+                    (<div>
+                        <div className="">
+                            {/* table upper part  */}
+
+                            <div className="overflow-x-auto">
+                                <table className="table">
+                                    {/* head */}
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Author</th>
+                                            <th>Category</th>
+                                            <th>Rating</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+
+                                    {
+                                        books.map((item, idx) =>
+                                            //    table body 
+
+                                            <tbody key={idx}>
+                                                {/* row 1 */}
+                                                <tr>
+                                                    <td>
+                                                        <div className="flex items-center gap-3">
+                                                            <div className="avatar">
+                                                                <div className="mask mask-squircle w-12 h-12">
+                                                                    <img src={item.image} />
+                                                                </div>
+                                                            </div>
+                                                            <div>
+                                                                <div className="font-bold">{item.name}</div>
+                                                            </div>
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        {item.author}
+                                                    </td>
+                                                    <td>{item.category}</td>
+                                                    <td>
+                                                        <Rating
+                                                            initialRating={item.rating}></Rating>
+                                                    </td>
+                                                    <th>
+                                                        <Link to={`/update-book/${item._id}`}>
+                                                            <button className="px-2 py-1 text-lg font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-accent rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">Update</button>
+                                                        </Link>
+                                                    </th>
+                                                </tr>
+                                            </tbody>
+
+
+                                        )
+                                    }
+                                    {/* table lower part  */}
+
+                                </table>
                             </div>
 
                         </div>
-                    )
-                }
-            </div>
 
+                    </div>)}
+                {/* List complete  */}
+            </div>
         </div>
+
     );
 };
 
 export default AllBooks;
+
+
+
+
+
+
+
+
+
+
